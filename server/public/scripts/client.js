ebf7875ae619dd2fdd12ed56778ccadcdc9abb33
Zeke Taves <ezekielprof@gmail.com>
2023-05-21 12:33:53 -0500

    Created a randomized quotes feature, and realized I am on the wrong branch again.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,19 @@
+// A list of quotes for a very important feature for footer of page.
+const quotes = [
+    "Productivity is overrated. I'm working on perfecting the art of looking busy instead.",
+    "My productivity is like a well-trained cat. It rarely shows up when I need it to.",
+    "Productivity tip: Procrastinate today so you can panic tomorrow.",
+    "I find that staring blankly into space really helps me visualize my productivity going down the drain.",
+    "I'm not lazy; I'm just conserving energy for future bursts of unproductivity.",
+    "My productivity levels are like a rollercoaster ride—lots of ups and downs, but mostly just going in circles.",
+    "They say time is money, so I guess that makes me a millionaire in wasted hours.",
+    "Productivity is a tricky thing. It's like trying to catch a unicorn—elusive and probably doesn't exist.",
+    "I've mastered the art of pretending to be busy. I should win an Oscar for my performances.",
+    "My productivity is like a rare species. Scientists have yet to discover any evidence of its existence."
+]
+
+const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
 // Activate jQuery
 $(() => {
     console.log('JQ JS');
@@ -13,6 +29,8 @@ $(() => {
     // END Click handlers
 
     // Preperations for page load.
+    // Display selected quote.
+    $('#importantQuote').text(selectedQuote);
 
     // Display items.
     callDisplay();
@@ -130,6 +148,7 @@ function removeTask(event) {
                 alert('Error doing it!');
             })
         } else {
+            // No procrastinating here.
             swal('Well fine then!');
         }
     })
